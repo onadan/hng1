4febc5f384f2eb4bfb30c4c00490227ad88950d5
onadan <onadan45@gmail.com>
2022-11-05 20:27:34 +0100

    contact route done

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -12,17 +12,20 @@ const Contact = () => {
 
         <form className=".contact__form">
           <div className="splitted">
-            <div className="fnW">
+            <div className="flex-col">
               <label htmlFor="firstName">First name</label>
               <input
                 type="text"
                 name="firstName"
                 id="first_name"
                 placeholder="Enter your first name"
+                required
               />
+
+              <p id="hint"></p>
             </div>
 
-            <div className="lnW">
+            <div className="flex-col">
               <label htmlFor="lastName">Last name</label>
               <input
                 type="text"
@@ -33,25 +36,24 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="eW">
+          <div className="flex-col">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your last name" />
+            <input type="email" id="email" placeholder="yourname@email.com" />
           </div>
 
-          <div className="taW">
-            <label htmlFor="message">First Name</label>
+          <div className="flex-col">
+            <label htmlFor="message">Message</label>
             <textarea
               name="message"
-              rows={10}
+              rows={8}
               id="message"
               placeholder="Send me a message and I'll reply you as soon as possible..."
             />
           </div>
 
-          <div className="aW">
+          <div className="accept">
             <input type="checkbox" name="accept" id="accept" />
             <label htmlFor="accept">
-              {" "}
               You agree to providing your data to {name} who may contact you
             </label>
           </div>
